5916fb911dab4803afe5680d12f3bee8b7d75a71
Farxod <rajabovfarxod2@gmail.com>
2022-04-17 16:30:17 +0500

    in vscode

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello post')
+    res.send('Hello posst')
 })
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
